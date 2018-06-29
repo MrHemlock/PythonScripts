873d47099a189e416807e4b59a6897fb3be88377
LordHemlock <browndj3@gmail.com>
2018-06-29 13:13:46 -0500

    Learning about event handling.

---
+++ Pyglet Testing/keyboardEvent.py
@@ -0,0 +1,16 @@
+import pyglet
+
+window = pyglet.window.Window()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print('A key was pressed')
+
+
+@window.event
+def on_draw():
+    window.clear()
+
+
+pyglet.app.run()
