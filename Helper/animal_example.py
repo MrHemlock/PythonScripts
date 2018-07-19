c392c2ee50bc2acbd34ed6fa08135c855237cf21
LordHemlock <browndj3@gmail.com>
2018-07-19 09:14:42 -0500

    Example of class inheritance using the Animal example

---
+++ Helper/animal_example.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self, alive=True):
+        self.alive = alive
+
+    def move(self):
+        print("I'm walkin', daddy!")
+
+
+class Mammal(Animal):
+    def __init__(self, fur=True, milk=True):
+        super().__init__()
+        self.fur = fur
+        self.milk = milk
+
+    def birth_live_young(self):
+        print("Just made myself an alive baby")
+
+
+if __name__ == "__main__":
+    dog = Mammal()
+    dog.move()
+    dog.birth_live_young()
