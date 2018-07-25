56f1635a4308591477a32d7d13e57364171d7ac8
LordHemlock <browndj3@gmail.com>
2018-07-25 15:45:45 -0500

    Clean up

--- Projects/inventory.py
+++ Projects/inventory.py
@@ -1,7 +1,8 @@
 # inventory.py
-#stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+# stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
-def displayInventory(inventory):
+
+def display_inventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
@@ -9,15 +10,16 @@ def displayInventory(inventory):
         item_total += v
     print("Total number of items: " + str(item_total))
 
-#displayInventory(stuff)
 
-def addToInventory(inventory, addedItems):
-    for item in addedItems:
+# displayInventory(stuff)
+def add_to_inventory(inventory, added_items):
+    for item in added_items:
         inventory.setdefault(item, 0)
         inventory[item] += 1
     return inventory
 
+
 inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-inv = addToInventory(inv, dragonLoot)
-displayInventory(inv)
+inv = add_to_inventory(inv, dragonLoot)
+display_inventory(inv)
--- bacon.py
+++ bacon.py
@@ -3,4 +3,3 @@ food_list = ["bacon", "Bacon"]
 
 if "pork" or food in food_list:
     print("Cool beans")
-    
